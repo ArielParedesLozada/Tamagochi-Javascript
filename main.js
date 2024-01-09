df88ca9d8089cd06ecdfe46992289aa56da763e8
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-01-08 19:50:57 -0500

    Se añade el botón para usar Observer Funciones para usar el observer

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { Observer } from './Entity/Observer.js';
 import { Tamagochi } from './Entity/Tamagochi.js';
+import { TamagochiVestido } from './Entity/DecoratorRopa.js';
 
+let vestido = false;
 let tamago;
 let vidaLlena = document.getElementById('vida-llena');
 let energiaLlena = document.getElementById('energia-llena');
@@ -9,61 +11,74 @@ let imagenTamagochi = document.getElementById('imagen-tamagochi');
 
 window.alimentar = function alimentar() {
     tamago.alimentar();
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.mimar = function mimar() {
     tamago.mimar();
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.golpear = function golpear() {
     tamago.golpear();
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.jugar = function jugar() {
     tamago.jugar();
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.curar = function curar() {
     tamago.curar();
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.muestraVida = function muestraVida() {
-    console.log(tamago.vida);
-    console.log(tamago.energia);
-    console.log(tamago.felicidad);
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
+    console.log(tamago.getEstado());
+}
+window.ropear = function ropear() {
+    if (vestido) {
+        tamago = tamago.getTamagochi();
+        vestido = false;
+    } else {
+        tamago = new TamagochiVestido(tamago);
+        vestido = true;
+    }
+    console.log(tamago.getVida());
+    console.log(tamago.getEnergia());
+    console.log(tamago.getFelicidad());
     console.log(tamago.getEstado());
 }
 window.creaTamagochi = function creaTamagochi() {
     const nombre = document.getElementById('nombre').value;
     tamago = new Tamagochi(nombre, 100, 100, 50, "neutral");
     let vidaObserver = new Observer(() => {
-        vidaLlena.style.width = `${tamago.vida}%`;
-        vidaLlena.innerText = `${tamago.vida}%`;
+        vidaLlena.style.width = `${tamago.getVida()}%`;
+        vidaLlena.innerText = `${tamago.getVida()}%`;
         vidaLlena.style.textAlign = 'right';
     });
     let energiaObserver = new Observer(() => {
-        energiaLlena.style.width = `${tamago.energia}%`;
-        energiaLlena.innerText = `${tamago.energia}%`;
+        energiaLlena.style.width = `${tamago.getEnergia()}%`;
+        energiaLlena.innerText = `${tamago.getEnergia()}%`;
         energiaLlena.style.textAlign = 'right';
     });
     let felicidadObserver = new Observer(() => {
-        felicidadLlena.style.width = `${tamago.felicidad}%`;
-        felicidadLlena.innerText = `${tamago.felicidad}%`;
+        felicidadLlena.style.width = `${tamago.getFelicidad()}%`;
+        felicidadLlena.innerText = `${tamago.getFelicidad()}%`;
         felicidadLlena.style.textAlign = 'right';
     });
     let imagenObserver = new Observer(() => {

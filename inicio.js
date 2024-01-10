4338a08c2031c75e5a6367793f1346413da2d5e3
ArielParedesLozada <152462807+ArielParedesLozada@users.noreply.github.com>
2024-01-09 23:44:51 -0500

    Se cambia el Javascript de index.html a inicio.js La lógica de crear y guardar al tamagochi está aquí

---
+++ inicio.js
@@ -0,0 +1,34 @@
+import { Tamagochi } from './Entity/Tamagochi.js';
+
+let botonCargar = document.getElementById('carga');
+let botonCrear = document.getElementById('crea');
+let nombreInput = document.getElementById('nombre');
+let tamagoGuardado = localStorage.getItem('tamagochi');
+if (tamagoGuardado === null) {
+    console.log("No hay tamagochis");
+    botonCrear.disabled = false;
+    nombreInput.disabled = false;
+    botonCargar.disabled = true;
+} else {
+    console.log(tamagoGuardado);
+    botonCrear.disabled = true;
+    nombreInput.disabled = true;
+    botonCargar.disabled = false;
+}
+
+window.cargaTamagochi = function cargaTamagochi() {
+    window.location.href = 'Tamagochi.html';
+}
+
+window.creaTamagochi = function creaTamagochi(){
+    let nombre = nombreInput.value;
+    let tamago = new Tamagochi(nombre,100,100,50,"neutral");
+    let tamagoJSON =JSON.stringify(tamago, (key, value) =>{
+        if (key === 'estado') {
+            return tamago.getEstado();
+        }
+        return value;
+    }) ;
+    localStorage.setItem('tamagochi', tamagoJSON);
+    window.location.href = 'Tamagochi.html';
+}
